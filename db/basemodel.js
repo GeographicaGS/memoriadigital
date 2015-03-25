22de2d2227a787be5b9881211f107f19a1da3f5b
Javier Aragón Encina <javier@MacBook-Pro-de-Javier.local>
2015-03-25 09:55:12 +0100

    correción de bug de la base de datos

--- db/basemodel.js
+++ db/basemodel.js
@@ -8,11 +8,11 @@ function BaseModel() {
 BaseModel.prototype.query = function(callback,query,parameters){
 	pg.connect(this._conString, function(err, db, done) {
 		db.query(query,parameters, function(err, result) {
-			// done();
+			done();
 			if(err) {
 	      		return console.error('error running query', err);
 	    	}
-	    	db.end();
+	    	// db.end();
 			callback(err,result.rows);
 		});
 	});
